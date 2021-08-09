7424bc9ca1a64c0bead690aeb8890f226ee7f3e2
Efe Bikmaz <bikmazefe@gmail.com>
2021-08-09 14:05:22 +0300

    enable uuid globally

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_095126) do
+ActiveRecord::Schema.define(version: 2021_08_09_110314) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
