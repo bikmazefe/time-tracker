412225b402109188d6c10ba92d95ac78aeb2ae3e
Efe Bikmaz <bikmazefe@gmail.com>
2021-08-12 18:03:27 +0300

    add dropdown menu to top bar

--- app/controllers/admin/entries_controller.rb
+++ app/controllers/admin/entries_controller.rb
@@ -2,7 +2,7 @@ class Admin::EntriesController < ApplicationController
   include AuthorizeAdminConcern
 
   def index
-    @entries = Entry.filter(params[:q])
+    @pagy, @entries = pagy(Entry.filter(params[:q]))
     @entry_types = Entry.all.collect { |e| e.entry_type }.uniq
     @users = User.all
   end
