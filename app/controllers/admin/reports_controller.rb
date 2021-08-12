b1f24c544774d3e5965b86648d4f14ca13fd0ab7
Efe Bikmaz <bikmazefe@gmail.com>
2021-08-13 02:24:18 +0300

    add csv format to reports

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,16 @@
 class Admin::ReportsController < ApplicationController
   def index
-    @pagy, @users = pagy(User.all)
+    @selected_users = params[:user_ids].present? ? User.where(id: params[:user_ids]) : User.all
+
+    respond_to do |format|
+      format.html do
+        @pagy, @users = pagy(@selected_users)
+      end
+      format.csv do
+        response.headers["Content-Type"] = "text/csv"
+        response.headers["Content-Disposition"] = "attachment; filename=Report-#{Date.today}.csv"
+        render :template => "admin/reports/index.csv.haml"
+      end
+    end
   end
 end
