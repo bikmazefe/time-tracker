bc000eada7c03fe222e4d9af83661933210cf45b
bikmazefe <bikmazefe@gmail.com>
2021-08-17 01:29:50 +0300

    add redirect path for successfull auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  private
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || profile_path
+  end
 end
