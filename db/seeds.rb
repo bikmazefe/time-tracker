547137fecb3a8f98a10c0b413a5a774f837e9e2e
bikmazefe <bikmazefe@gmail.com>
2021-08-17 14:44:34 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# users = User.create([
-#   {
-#     email: "user1@example.com",
-#     password: "12345678",
-#   },
-#   {
-#     email: "user2@example.com",
-#     password: "12345678",
-#   },
-# ])
+admin = User.create(email: "admin@example.com", password: "12345678", admin: true)
+
+users = User.create([
+  {
+    email: "user1@example.com",
+    password: "12345678",
+  },
+  {
+    email: "user2@example.com",
+    password: "12345678",
+  },
+  {
+    email: "user3@example.com",
+    password: "12345678",
+  },
+])
+
+entries = Entry.create([
+  {
+    user: users[0],
+    entry_type: "Personal",
+    started_at: DateTime.new(2021, 8, 17, 12, 21),
+    finished_at: DateTime.new(2021, 8, 17, 12, 27),
+  },
+  {
+    user: users[0],
+    entry_type: "Work",
+    started_at: DateTime.new(2021, 8, 17, 16, 18),
+    finished_at: DateTime.new(2021, 8, 17, 16, 44),
+  },
+  {
+    user: users[1],
+    entry_type: "Work",
+    started_at: DateTime.new(2021, 8, 17, 9, 36),
+    finished_at: DateTime.new(2021, 8, 17, 9, 51),
+  },
+  {
+    user: users[1],
+    entry_type: "Work",
+    started_at: DateTime.new(2021, 8, 17, 10, 14),
+    finished_at: DateTime.new(2021, 8, 17, 11, 2),
+  },
+  {
+    user: users[2],
+    entry_type: "Work",
+    started_at: DateTime.new(2021, 8, 17, 9, 21),
+    finished_at: DateTime.new(2021, 8, 17, 9, 34),
+  },
+  {
+    user: users[2],
+    entry_type: "Personal",
+    started_at: DateTime.new(2021, 8, 17, 10, 3),
+    finished_at: DateTime.new(2021, 8, 17, 10, 28),
+  },
+])
